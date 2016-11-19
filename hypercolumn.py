11aea5a5b69dc45ab6ceca755091a7546ce0c513
AlexandreRobicquet <robicquet.alexandre@gmail.com>
2016-11-19 15:24:31 -0800

    change hypercolumn.py

--- hypercolumn.py
+++ hypercolumn.py
@@ -56,3 +56,21 @@ def VGG_16(weights_path=None):
     if weights_path:
         model.load_weights(weights_path)
     return model
+
+
+
+
+model = VGG_16('vgg16_weights.h5')
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(optimizer=sgd, loss='categorical_crossentropy')
+
+
+im_original = cv2.resize(cv2.imread('madruga.jpg'), (224, 224))
+im = im_original.transpose((2,0,1))
+im = np.expand_dims(im, axis=0)
+im_converted = cv2.cvtColor(im_original, cv2.COLOR_BGR2RGB)
+plt.imshow(im_converted)
+
+
+out = model.predict(im)
+plt.plot(out.ravel())
