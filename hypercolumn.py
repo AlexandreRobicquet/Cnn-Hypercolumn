dadf25396c5230b67e2c886b2feaadb036561d66
AlexandreRobicquet <robicquet.alexandre@gmail.com>
2016-11-26 10:11:10 -0800

    minor correction;

--- hypercolumn.py
+++ hypercolumn.py
@@ -70,23 +70,3 @@ def extract_hypercolumn(model, layer_indexes, instance):
                                         mode="F", interp='bilinear')
             hypercolumns.append(upscaled)
     return np.asarray(hypercolumns)
-
-
-
-
-
-
-
-
-def extract_hypercolumn(model, layer_indexes, instance):
-    layers = [model.layers[li].get_output(train=False) for li in layer_indexes]
-    get_feature = theano.function([model.layers[0].input], layers,
-                                  allow_input_downcast=False)
-    feature_maps = get_feature(instance)
-    hypercolumns = []
-    for convmap in feature_maps:
-        for fmap in convmap[0]:
-            upscaled = sp.misc.imresize(fmap, size=(224, 224),
-                                        mode="F", interp='bilinear')
-            hypercolumns.append(upscaled)
-    return np.asarray(hypercolumns)
